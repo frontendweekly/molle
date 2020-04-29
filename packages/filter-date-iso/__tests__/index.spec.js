5c05bd079f4fb4fa3082ef473ee685ef01ab0cc8
Yuya Saito <studiomohawk@gmail.com>
2020-04-29 20:27:46 +0900

    fix: swap test

--- packages/filter-date-iso/__tests__/index.spec.js
+++ packages/filter-date-iso/__tests__/index.spec.js
@@ -1,49 +1,16 @@
 const {advanceTo, clear} = require('jest-date-mock');
 
-const dateWithOrdinalSuffixFilter = require('../index');
+const dateISO = require('../index');
 
-describe('filterDateWithOrdinalSuffix', () => {
-  test('it should add ordinal suffix st to given date', () => {
+describe('dateISO', () => {
+  test('it should output date in ISO format', () => {
     // Arrange
     advanceTo(new Date(2020, 3, 1, 0, 0, 0));
     const date = Date.now();
     // Act
-    const actual = dateWithOrdinalSuffixFilter(date);
+    const actual = dateISO(date);
     // Assert
-    const expected = `1st April 2020`;
-    expect(actual).toEqual(expected);
-  });
-
-  test('it should add ordinal suffix nd to given date', () => {
-    // Arrange
-    advanceTo(new Date(2020, 3, 22, 0, 0, 0));
-    const date = Date.now();
-    // Act
-    const actual = dateWithOrdinalSuffixFilter(date);
-    // Assert
-    const expected = `22nd April 2020`;
-    expect(actual).toEqual(expected);
-  });
-
-  test('it should add ordinal suffix rd to given date', () => {
-    // Arrange
-    advanceTo(new Date(2020, 3, 3, 0, 0, 0));
-    const date = Date.now();
-    // Act
-    const actual = dateWithOrdinalSuffixFilter(date);
-    // Assert
-    const expected = `3rd April 2020`;
-    expect(actual).toEqual(expected);
-  });
-
-  test('it should add ordinal suffix th to given date', () => {
-    // Arrange
-    advanceTo(new Date(2020, 3, 29, 0, 0, 0));
-    const date = Date.now();
-    // Act
-    const actual = dateWithOrdinalSuffixFilter(date);
-    // Assert
-    const expected = `29th April 2020`;
+    const expected = `2020-03-31T15:00:00.000Z`;
     expect(actual).toEqual(expected);
   });
 });
