7a7aa0b64da718c71ff1edee3990df0c9e94161c
Yuya Saito <studiomohawk@gmail.com>
2020-05-02 16:23:52 +0900

    feat: import newly created filters

--- packages/molle/.eleventy.js
+++ packages/molle/.eleventy.js
@@ -1,5 +1,8 @@
 const filterDateOrdinalSuffix = require('@frontendweekly/filter-date-ordinal-suffix');
 const filterDateIso = require('@frontendweekly/filter-date-iso');
+const filterHead = require('@frontendweekly/filter-head');
+const filterTagsToSentence = require('@frontendweekly/filter-tags-to-sentence');
+const filterMarkdown = require('@frontendweekly/filter-markdown');
 
 const transformHtmlMin = require('@frontendweekly/transform-htmlmin');
 const transformEnhancePostHtml = require('@frontendweekly/transform-enhance-post-html');
@@ -7,6 +10,9 @@ const transformEnhancePostHtml = require('@frontendweekly/transform-enhance-post
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('dateOrdinalSuffixFilter', filterDateOrdinalSuffix);
   eleventyConfig.addFilter('dateIsoFilter', filterDateIso);
+  eleventyConfig.addFilter('head', filterHead);
+  eleventyConfig.addFilter('tagsToSentence', filterTagsToSentence);
+  eleventyConfig.addFilter('markdownFilter', filterMarkdown);
   eleventyConfig.addFilter('jsonify', (value) => JSON.stringify(value));
   eleventyConfig.addTransform('htmlmin', transformHtmlMin);
   eleventyConfig.addTransform('enhancePostHtml', transformEnhancePostHtml);
