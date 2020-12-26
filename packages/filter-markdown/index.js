9331c35abac1a12da38f9b4e87c475fc02a0729a
Yuya Saito <hello@studiomohawk.com>
2020-12-26 21:45:52 +0900

    fix: add test

--- packages/filter-markdown/index.js
+++ packages/filter-markdown/index.js
@@ -1,10 +1,5 @@
-const markdownIt = require('markdown-it')({
-  html: true,
-  breaks: true,
-  linkify: true,
-  typographer: true,
-});
+const md = require('@frontendweekly/eleventy-plugin-markdown');
 
 module.exports = function markdown(value) {
-  return markdownIt.render(value);
+  return md.render(value);
 };
