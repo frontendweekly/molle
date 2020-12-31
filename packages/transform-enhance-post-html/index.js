cb4e87fb08b4403093670765907e92cbc549eb1a
Yuya Saito <hello@studiomohawk.com>
2020-12-31 23:35:08 +0900

    feat: drop permalink

--- packages/transform-enhance-post-html/index.js
+++ packages/transform-enhance-post-html/index.js
@@ -3,7 +3,8 @@ const {JSDOM} = jsdom;
 const slugify = require('slugify');
 
 const minify = (input) => input.replace(/\s{2,}/g, '').replace(/'/g, '"');
-const shouldTransformHTML = (outputPath) => outputPath && outputPath.endsWith('.html');
+const shouldTransformHTML = (outputPath) =>
+  outputPath && outputPath.endsWith('.html');
 
 module.exports = function (content, outputPath) {
   if (shouldTransformHTML(outputPath)) {
@@ -19,7 +20,9 @@ module.exports = function (content, outputPath) {
       ),
     ];
     const articleEmbeds = [...document.querySelectorAll('.c-post iframe')];
-    const articleCodeBlocks = [...document.querySelectorAll('.c-post pre[class]')];
+    const articleCodeBlocks = [
+      ...document.querySelectorAll('.c-post pre[class]'),
+    ];
 
     if (articleImages.length) {
       articleImages.forEach((image) => {
@@ -37,25 +40,6 @@ module.exports = function (content, outputPath) {
           figure.appendChild(figCaption);
           image.replaceWith(figure);
         }
-      });
-    }
-
-    if (articleHeadings.length) {
-      // Loop each heading and add a little anchor and an ID to each one
-      articleHeadings.forEach((heading) => {
-        const headingSlug = slugify(heading.textContent.toLowerCase());
-        const anchor = document.createElement('a');
-
-        anchor.setAttribute('href', `#heading-${headingSlug}`);
-        anchor.classList.add('c-heading-permalink');
-        anchor.innerHTML = minify(`
-        <span class="u-visually-hidden"> permalink</span>
-        <svg fill="currentColor" aria-hidden="true" focusable="false" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M9.199 13.599a5.99 5.99 0 0 0 3.949 2.345 5.987 5.987 0 0 0 5.105-1.702l2.995-2.994a5.992 5.992 0 0 0 1.695-4.285 5.976 5.976 0 0 0-1.831-4.211 5.99 5.99 0 0 0-6.431-1.242 6.003 6.003 0 0 0-1.905 1.24l-1.731 1.721a.999.999 0 1 0 1.41 1.418l1.709-1.699a3.985 3.985 0 0 1 2.761-1.123 3.975 3.975 0 0 1 2.799 1.122 3.997 3.997 0 0 1 .111 5.644l-3.005 3.006a3.982 3.982 0 0 1-3.395 1.126 3.987 3.987 0 0 1-2.632-1.563A1 1 0 0 0 9.201 13.6zm5.602-3.198a5.99 5.99 0 0 0-3.949-2.345 5.987 5.987 0 0 0-5.105 1.702l-2.995 2.994a5.992 5.992 0 0 0-1.695 4.285 5.976 5.976 0 0 0 1.831 4.211 5.99 5.99 0 0 0 6.431 1.242 6.003 6.003 0 0 0 1.905-1.24l1.723-1.723a.999.999 0 1 0-1.414-1.414L9.836 19.81a3.985 3.985 0 0 1-2.761 1.123 3.975 3.975 0 0 1-2.799-1.122 3.997 3.997 0 0 1-.111-5.644l3.005-3.006a3.982 3.982 0 0 1 3.395-1.126 3.987 3.987 0 0 1 2.632 1.563 1 1 0 0 0 1.602-1.198z"/>
-        </svg>`);
-
-        heading.setAttribute('id', `heading-${headingSlug}`);
-        heading.appendChild(anchor);
       });
     }
 
